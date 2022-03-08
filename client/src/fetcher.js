b9405b302eae5037961c39c084367d4c7e387e38
irwin-deng <85715371+irwin-deng@users.noreply.github.com>
2022-03-08 15:13:33 -0500

    Updated fetcher.js
    Added support for server addresses where a port is not specified

--- client/src/fetcher.js
+++ client/src/fetcher.js
@@ -1,42 +1,45 @@
 import config from './config.json'
+const base_url = (config.server_port == null) ?
+                 `http://${config.server_host}` :
+                 `http://${config.server_host}:${config.server_port}`
 
 const getAllMatches = async (page, pagesize, league) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/matches/${league}?page=${page}&pagesize=${pagesize}`, {
+    var res = await fetch(`${base_url}/matches/${league}?page=${page}&pagesize=${pagesize}`, {
         method: 'GET',
     })
     return res.json()
 }
 
 const getAllPlayers = async (page, pagesize) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/players?page=${page}&pagesize=${pagesize}`, {
+    var res = await fetch(`${base_url}/players?page=${page}&pagesize=${pagesize}`, {
         method: 'GET',
     })
     return res.json()
 }
 
 const getMatch = async (id) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/match?id=${id}`, {
+    var res = await fetch(`${base_url}/match?id=${id}`, {
         method: 'GET',
     })
     return res.json()
 }
 
 const getPlayer = async (id) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/player?id=${id}`, {
+    var res = await fetch(`${base_url}/player?id=${id}`, {
         method: 'GET',
     })
     return res.json()
 }
 
 const getMatchSearch = async (home, away, page, pagesize) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/matches?Home=${home}&Away=${away}&page=${page}&pagesize=${pagesize}`, {
+    var res = await fetch(`${base_url}/search/matches?Home=${home}&Away=${away}&page=${page}&pagesize=${pagesize}`, {
         method: 'GET',
     })
     return res.json()
 }
 
 const getPlayerSearch = async (name, nationality, club, rating_high, rating_low, pot_high, pot_low, page, pagesize) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/players?Name=${name}&Nationality=${nationality}&Club=${club}&RatingLow=${rating_low}&RatingHigh=${rating_high}&PotentialHigh=${pot_high}&PotentialLow=${pot_low}&page=${page}&pagesize=${pagesize}`, {
+    var res = await fetch(`${base_url}/search/players?Name=${name}&Nationality=${nationality}&Club=${club}&RatingLow=${rating_low}&RatingHigh=${rating_high}&PotentialHigh=${pot_high}&PotentialLow=${pot_low}&page=${page}&pagesize=${pagesize}`, {
         method: 'GET',
     })
     return res.json()
